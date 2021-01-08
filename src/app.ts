b29f427b4d489d6c08f6cd0469b56974010cb5a3
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-08 10:39:17 -0300

    removed unecessary commetary

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts'; // Oak framework
+import { Application } from 'https://deno.land/x/oak/mod.ts';
 import router from './routes.ts';
 
 const app: Application = new Application();
